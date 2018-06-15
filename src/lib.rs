2f1eb5597c91f4174227741c482a403cc836b44b
Sean Bruton <sean@uberfoo.net>
2018-06-15 02:56:19 -0500

    initial import

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+extern crate prost;
+#[macro_use]
+extern crate prost_derive;
+extern crate prost_types;
+
+// Include the sensor elements, which are generated from sensor.proto.
+include!(concat!(env!("OUT_DIR"), "/sensor.rs"));
